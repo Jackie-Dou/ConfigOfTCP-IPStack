f43f0616fd2199534de8d11e96fc0fd68ca04281
Jackie-Dou <57232952+Jackie-Dou@users.noreply.github.com>
2021-01-27 22:39:58 +0300

    Add files via upload

---
+++ Client/Client/Model/SettingServer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Model
+{
+    public class SettingServer
+    {
+        public string IP { get; set; }
+        public int Host { get; set; }
+        public Color ColorText { get; set; }
+        public Color ColorFrons { get; set; }
+        public int HigthText { get; set; }
+
+        public SettingServer()
+        {
+            ColorFrons = Color.White;
+            ColorText = Color.Black;
+            HigthText = 8;
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adrress = null;
+            int port = 0;
+            string temp = null;
+            bool flag = false;
+            Console.WriteLine("Введите IP-адрес:");
+            temp = Console.ReadLine();
+            if (IsDigitsAndDotOnly(temp)) { 
+                adrress = temp;
+                flag = true;
+            }
+            Console.WriteLine("Введите порт:");
+            temp = Console.ReadLine();
+            if (IsDigitsOnly(temp)) { 
+                port = int.Parse(temp);
+                flag = true;
+            }
+            if (flag == true) { 
+                TCPServer server = new TCPServer();
+                server.Create(adrress, port);
+                server.InfoServer();
+                server.StartServer();
+            }
+        }
+
+        static bool IsDigitsOnly(string str)
+        {
+            foreach (char c in str)
+            {
+                if (c < '0' || c > '9')
+                    return false;
+            }
+
+            return true;
+        }
+
+        static bool IsDigitsAndDotOnly(string str)
+        {
+            foreach (char c in str)
+            {
+                if ((c < '0' || c > '9') && c!='.')
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
